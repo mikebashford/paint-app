eee41c6e821b666d5e522cf2a00625a5afed948b
Michael Bashford <mikebashford@gmail.com>
2022-05-27 23:05:31 -0500

    Added the ability to change the shape of the brush

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
 const canvas = document.querySelector(".canvas");
 const clearButton = document.querySelector(".clear");
+const shapes = document.querySelector(".shapes");
 
 let isDrawing = false;
+let shape = "circleOption";
 
 canvas.addEventListener("mousedown", (e) => {
   paintSpot(e.pageX, e.pageY);
@@ -24,12 +26,16 @@ canvas.addEventListener("mousemove", (e) => {
 
 function paintSpot(x, y) {
   const spot = document.createElement("div");
-  spot.className = "spot circleOption";
+  spot.className = `spot ${shape}`;
   spot.style.left = `${x - 15}px`;
   spot.style.top = `${y - 15}px`;
   spot.style.background = "lightblue";
   canvas.appendChild(spot);
 }
+
+shapes.addEventListener("click", (e) => {
+  if (e.target.className != "shapes") shape = e.target.className;
+});
 
 clearButton.addEventListener("click", () => {
   canvas.innerHTML = "";
