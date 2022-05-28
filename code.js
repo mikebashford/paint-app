a9320788cc1bc36665c7a644b70b022b9e0faa0c
Michael Bashford <mikebashford@gmail.com>
2022-05-27 23:13:29 -0500

    Added functionality to change color painted on canvas

--- code.js
+++ code.js
@@ -1,9 +1,11 @@
 const canvas = document.querySelector(".canvas");
 const clearButton = document.querySelector(".clear");
 const shapes = document.querySelector(".shapes");
+const colorInput = document.querySelector(".color");
 
 let isDrawing = false;
 let shape = "circleOption";
+let color = "black";
 
 canvas.addEventListener("mousedown", (e) => {
   paintSpot(e.pageX, e.pageY);
@@ -29,7 +31,7 @@ function paintSpot(x, y) {
   spot.className = `spot ${shape}`;
   spot.style.left = `${x - 15}px`;
   spot.style.top = `${y - 15}px`;
-  spot.style.background = "lightblue";
+  spot.style.background = color;
   canvas.appendChild(spot);
 }
 
@@ -39,4 +41,11 @@ shapes.addEventListener("click", (e) => {
 
 clearButton.addEventListener("click", () => {
   canvas.innerHTML = "";
+});
+
+colorInput.addEventListener("change", (e) => {
+  color = e.target.value;
+  for (let i = 0; i < shapes.children.length; i++) {
+    shapes.children[i].style.background = color;
+  }
 });
