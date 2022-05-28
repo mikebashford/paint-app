b44c3790af85246eef9f1b1c4b7b45bc080e1286
Michael Bashford <mikebashford@gmail.com>
2022-05-27 22:51:17 -0500

    Added ability to paint while mouse button is down and moving on the canvas

--- code.js
+++ code.js
@@ -3,24 +3,29 @@ const canvas = document.querySelector(".canvas");
 let isDrawing = false;
 
 canvas.addEventListener("mousedown", (e) => {
+  paintSpot(e.pageX, e.pageY);
   isDrawing = true;
 });
 
-canvas.addEventListener("mouseup", (e) => {
+canvas.addEventListener("mouseup", () => {
   isDrawing = false;
 });
 
-canvas.addEventListener("mouseleave", (e) => {
+canvas.addEventListener("mouseleave", () => {
   isDrawing = false;
 });
 
-paintSpot(100, 100);
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    paintSpot(e.pageX, e.pageY);
+  }
+});
 
 function paintSpot(x, y) {
   const spot = document.createElement("div");
   spot.className = "spot circleOption";
-  spot.style.left = `${x}px`;
-  spot.style.top = `${y}px`;
+  spot.style.left = `${x - 15}px`;
+  spot.style.top = `${y - 15}px`;
   spot.style.background = "lightblue";
   canvas.appendChild(spot);
 }
